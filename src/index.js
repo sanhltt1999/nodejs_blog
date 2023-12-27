b856f18a1ccb3c172ae55efdb5e69a3320b703ac
Shank <sanhltt1999@gmail.com>
2023-12-27 14:38:55 +0700

    add prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars.engine({extname:'.hbs'}));
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
